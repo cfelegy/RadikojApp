0a56cfce126241dd6812a0c52a0f2393cdabd807
Christopher Felegy <chris@felegy.net>
2021-10-07 22:58:47 -0400

    feature: add view and route to display an article
    Also added a route to load in a debug article, will be removed before
    production deployment.

--- wg-decanonization-gasp/Controllers/ArticlesController.cs
+++ wg-decanonization-gasp/Controllers/ArticlesController.cs
@@ -1,5 +1,8 @@
 ﻿using GaspApp.Data;
+using GaspApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace GaspApp.Controllers
 {
@@ -21,6 +24,49 @@ namespace GaspApp.Controllers
             };
 
             return View(model);
+        }
+
+        [Route("[controller]/{slug}")]
+        public IActionResult Article(string slug)
+        {
+            var article = _dbContext.Articles.SingleOrDefault(x => x.Slug == slug);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Entry(article).Collection(x => x.Contents).Load();
+            
+            // TODO: ArticlesArticleViewModel to localize body; just debugging for now
+            return View(article);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AddDebugArticles()
+        {
+            var account = await _dbContext.Accounts.FindAsync(new Guid(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value));
+            var contentEn = new ArticleContent
+            {
+                Culture = "en-US", // hard-coding like this is bad
+                Title = "Test Article 1",
+                Body = "Contents written for test article 1..."
+            };
+            var contentEs = new ArticleContent
+            {
+                Culture = "es",
+                Title = "Aritculo de Prueba 1",
+                Body = "Los contenidos para el articulo de prueba 1..."
+            };
+            var article = new Article
+            {
+                Slug = "test-1",
+                Author = account,
+                Contents = new List<ArticleContent> { contentEn, contentEs },
+                PublishedDate = DateTimeOffset.Now.ToUniversalTime(),
+            };
+            await _dbContext.Articles.AddAsync(article);
+            await _dbContext.SaveChangesAsync();
+            
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ wg-decanonization-gasp/Migrations/20211008023045_articles.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GaspApp.Migrations
+{
+    public partial class articles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    PublishedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Slug = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articles_Accounts_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleContent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Culture = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    ArticleId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleContent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ArticleContent_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleContent_ArticleId",
+                table: "ArticleContent",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleContent");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
